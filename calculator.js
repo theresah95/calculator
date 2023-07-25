692e9d99df132dfdb4153029b56d8c59d774ccc5
TheresaH <44990136+theresah95@users.noreply.github.com>
2023-07-25 10:33:15 -0700

    syntax changes for strings

--- calculator.js
+++ calculator.js
@@ -33,7 +33,7 @@ let num1 = 0;
 //Second number entered in calculator
 let num2 = 0;
 //Operator entered in calculator
-let operator = "";
+let operator = '';
 
 /** Operate Function
  * @param {number} num1 
@@ -45,13 +45,13 @@ function operate (num1, num2, operator) {
     
     let result = 0;
 
-    if(operator == "+"){
+    if(operator == '+'){
         result = add(num1, num2);
-    } else if (operator == "-"){
+    } else if (operator == '-'){
         result = subtract(num1, num2);
-    } else if (operator == "*"){
+    } else if (operator == '*'){
         result = multiply(num1, num2);
-    } else if (operator == "/"){
+    } else if (operator == '/'){
         result = divide(num1, num2);
     }
 
